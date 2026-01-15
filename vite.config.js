a45d0d7f3775a4314d602a84a773fc355fb682ac
Angwe Chima <angwechima@gmail.com>
2026-01-15 17:40:41 +0100

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    base: '/digital-asset-manager/',
+  base: process.env.NODE_ENV === 'production' ? '/digital-asset-manager/' : '/',
 });
